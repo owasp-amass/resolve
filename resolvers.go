0a1aebc6904c665aed533c19a9d0d1496c26ebfd
caffix <caffix@users.noreply.github.com>
2022-11-27 14:44:05 -0500

    Removed the read deadline

--- resolvers.go
+++ resolvers.go
@@ -334,7 +334,6 @@ func (r *resolver) responses() {
 }
 
 func (r *resolver) read() {
-	_ = r.conn.SetReadDeadline(time.Now().Add(3 * time.Second))
 	if m, err := r.conn.ReadMsg(); err == nil && m != nil && len(m.Question) > 0 {
 		if req := r.xchgs.remove(m.Id, m.Question[0].Name); req != nil {
 			if m.Truncated {
